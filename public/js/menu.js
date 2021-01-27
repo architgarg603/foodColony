2932735a08f8ca1a856bee015e23c7230beca714
Archit garg <architgarg603@gmail.com>
2021-01-27 20:53:44 +0530

    changed

--- public/js/menu.js
+++ public/js/menu.js
@@ -10,42 +10,42 @@ let tea = document.querySelector(".dish7");
 let drink = document.querySelector(".dish8");
 let item = document.querySelectorAll(".item");
 let order = [];
-window.addEventListener("load",function(){
-    
-    for(let i=0;i<dishes.length;i++){
-        let checkbtn =  dishes[i].querySelector(".added");
-       checkbtn.addEventListener("click",async function(){
+window.addEventListener("load", function () {
 
-            if(checkbtn.innerHTML == "✓"){
+    for (let i = 0; i < dishes.length; i++) {
+        let checkbtn = dishes[i].querySelector(".added");
+        checkbtn.addEventListener("click", async function () {
+
+            if (checkbtn.innerHTML == "✓") {
                 let name = dishes[i].querySelector(".itemName").innerHTML;
                 let amt = dishes[i].querySelector(".amt").innerHTML;
-                order = order.filter((x)=>{
-                    return x.name!=name && x.amt!=amt;
+                order = order.filter((x) => {
+                    return x.name != name && x.amt != amt;
                 })
                 checkbtn.innerHTML = "+";
-            }else{
-                order.push({"name":dishes[i].querySelector(".itemName").innerHTML, "img":dishes[i].querySelector(".img img").getAttribute("src"),"amt":dishes[i].querySelector(".amt").innerHTML})
+            } else {
+                order.push({ "name": dishes[i].querySelector(".itemName").innerHTML, "img": dishes[i].querySelector(".img img").getAttribute("src"), "amt": dishes[i].querySelector(".amt").innerHTML })
                 checkbtn.innerHTML = "✓";
-            }   
+            }
         })
     }
-    cartbtn.addEventListener("click",async function(){
-        let details = await axios.post("https://foodcolony.herokuapp.com/order/cart/add",{
-            "cart":order
+    cartbtn.addEventListener("click", async function () {
+        let details = await axios.post("https://foodcolony.herokuapp.com/order/cart/add", {
+            "cart": order
         });
         window.location.href = "/cart"
     })
 
-    $(document).ready(function(){
-        $('.dish').click(function(){
-            const value = $(this).attr('data-filter');
-            if(value == 'all'){
-                $('.item').show('1000');
-            }else{
-                $('.item').not('.'+value).hide('1000');   
-                $('.item').filter('.'+value).show('1000');   
-            }
-        })
-    })
+})
 
+$(document).ready(function () {
+    $('.dish').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.item').show('1000');
+        } else {
+            $('.item').not('.' + value).hide('1000');
+            $('.item').filter('.' + value).show('1000');
+        }
+    })
 })
